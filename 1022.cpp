7c4a6afc18e0d012127d5463a253ec4075735fb1
김성민 <tolelom@mail.hongik.ac.kr>
2021-05-15 17:04:14 +0900

    Create 1022.cpp

---
+++ 1022.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    getline(cin, s);
+    cout << s;   
+}
