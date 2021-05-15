b371e441776dab53655f4ed3ca061f1085b61239
김성민 <tolelom@mail.hongik.ac.kr>
2021-05-15 16:43:04 +0900

    Create 1018.cpp

---
+++ 1018.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int h, m;
+    char c;
+    cin >> h >> c >> m;
+    cout << h << c << m << '\n';
+}
