ad822954374cbc2a6f0b37000f0fdfb902ffeb28
김성민 <tolelom@mail.hongik.ac.kr>
2021-05-15 16:05:27 +0900

    Create 1017.cpp

---
+++ 1017.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a;
+    cin >> a;
+    cout << a << ' ' << a << ' ' << a << '\n';
+}
