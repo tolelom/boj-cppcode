ad477527d01f6e2605c37bc78d353344965e65be
김성민 <tolelom@mail.hongik.ac.kr>
2021-05-15 17:28:42 +0900

    Create 1023.cpp

---
+++ 1023.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int a, b;
+    char c;
+    cin >> a >> c >> b;
+    cout << a << '\n';
+    cout << b << '\n';
+}
