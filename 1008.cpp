89e5cac36628e9c32dff7733f8dab6c5af384545
김성민 <tolelom@mail.hongik.ac.kr>
2021-05-15 15:48:05 +0900

    Create 1008.cpp

---
+++ 1008.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << "\u250C\u252C\u2510\n";
+    cout << "\u251C\u253C\u2524\n";
+    cout << "\u2514\u2534\u2518\n";
+}
